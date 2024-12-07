7a4b1de98cba8541f8552797f8e262030c79d720
knaka <k.nakamura.2003@gmail.com>
2024-12-07 10:40:16 +0900

    1-5練習問題

---
+++ src/Practice02.java
@@ -0,0 +1,9 @@
+public class Practice02 {
+    public static void main(String[] args) {
+        boolean flag = true;
+        char initial = '駆';
+        double pi = 3.14;
+        long water = 314159265853979L;
+        String attack = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+    }
+}
