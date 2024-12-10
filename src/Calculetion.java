e4cd2e144852454ac34f994df4a6cf5f4f32fecb
knaka <k.nakamura.2003@gmail.com>
2024-12-10 10:57:04 +0900

    2.3,2.4の履修

--- src/Calculetion.java
+++ src/Calculetion.java
@@ -8,5 +8,20 @@ public class Calculetion {
         System.out.println(b);
         System.out.println("私の好きな記号は二重引用符(\") です");
         System.out.println("お年玉は\\12000です");
+        int c;
+        c = 3 + 5 * 6;
+        System.out.println(c);
+        System.out.println("こん" + "にちは");
+        int d;
+        d = 9 / 2;
+        System.out.println(d);
+        double e;
+        e = 9.0 / 2;
+        System.out.println(e);
+        int f;
+        f = 100;
+        f++;
+        f--;
+        System.out.println(f);
     }
 }
