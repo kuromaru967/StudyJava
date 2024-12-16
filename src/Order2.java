a72b103fc62d8e8ec9f683dc49298e729e0cd189
knaka <k.nakamura.2003@gmail.com>
2024-12-17 08:48:01 +0900

    2.6.4~2.6.6の履修(フォーマット済) ・クラス名の2が全角だったので半角に直した ・オブジェクトの生成（new）を1度だけにした

--- src/Order2.java
+++ src/Order2.java
@@ -1,9 +1,12 @@
-public class Order２ {
+import java.util.Scanner;
+
+public class Order2 {
     public static void main(String[] args) {
         System.out.println("あなたの名前を入力してください。");
-        String name = new java.util.Scanner(System.in).nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
         System.out.println("あなたの年齢を入力してください。");
-        int age = new java.util.Scanner(System.in).nextInt();
+        int age = scanner.nextInt();
         System.out.println
                 ("ようこそ" + age + "歳の" + name + "さん");
     }
