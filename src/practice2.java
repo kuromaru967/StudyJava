aefbf0be1fadb9603af71db1f9011302bd83db53
knaka <k.nakamura.2003@gmail.com>
2024-12-18 10:44:03 +0900

    2.8の練習問題

---
+++ src/practice2.java
@@ -0,0 +1,29 @@
+public class practice2 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        int z = x + y;
+//        String ans = "x+yは" + (x + y);
+        String ans = "x+yは" + z;
+        System.out.println(ans);
+
+        practice22();
+    }
+
+    private static void practice22() {
+        // ①×
+//        int x = 3 + 5.0;
+        // ②○
+//        double d = 2.0F;
+        // ➂×
+//        int i = "5";
+        // ④○
+//        String s = 2 + "人目";
+        // ⑤〇
+//        byte b = 1;
+        // ⑥×
+//        double d = true;
+        // ⑦〇
+//        short s = (byte)2;
+    }
+}
