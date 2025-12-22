6908fb54e4eec0562da471d8e1bf4454d8d09d6c
knaka <k.nakamura.2003@gmail.com>
2025-12-22 18:12:49 +0900

    printlnの不要な空の文字列引数を削除

--- src/Nest.java
+++ src/Nest.java
@@ -5,7 +5,7 @@ public class Nest {
                 System.out.print(i * j);  //掛け算の結果を出力
                 System.out.print(" ");    //空白を出力
             }
-            System.out.println("");       //改行を出力
+            System.out.println();       //改行を出力
         }
     }
 }
