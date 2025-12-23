20181309cc62a6677a63d382a17a8284dc422651
knaka <k.nakamura.2003@gmail.com>
2025-12-23 18:24:38 +0900

    3.6.2の履修、3.8.1-3.8.3の練習問題

--- src/Study_3_8_1_2.java
+++ src/Study_3_8_1_2.java
@@ -5,21 +5,21 @@ public class Study_3_8_1_2 {
         Scanner scanner = new Scanner(System.in);
 
         // ①
-//        int weight = scanner.nextInt();
-//        System.out.println(weight == 60);
+        int weight = scanner.nextInt();
+        System.out.println(weight == 60);
 
         // ➁
-//        int age1 = scanner.nextInt();
-//        int age2 = scanner.nextInt();
-//        System.out.println((age1 + age2) * 2 > 60);
+        int age1 = scanner.nextInt();
+        int age2 = scanner.nextInt();
+        System.out.println((age1 + age2) * 2 > 60);
 
         // ③
-//        int age = scanner.nextInt();
-//        System.out.println(age % 2 == 1);
+        int age = scanner.nextInt();
+        System.out.println(age % 2 == 1);
 
         // ④
-//        String name = scanner.nextLine();
-//        System.out.println("湊".equals(name));
+        String name = scanner.nextLine();
+        System.out.println("湊".equals(name));
 
         //3-2 A.C.E.F
     }
