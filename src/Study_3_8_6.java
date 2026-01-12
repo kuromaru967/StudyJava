7741502217004985bc854452b0291c3d075dc5ca
knaka <k.nakamura.2003@gmail.com>
2026-01-12 11:43:22 +0900

    3.8.6の修正（コメントの追加）

--- src/Study_3_8_6.java
+++ src/Study_3_8_6.java
@@ -1,9 +1,11 @@
 public class Study_3_8_6 {
     public static void main(String[] args) {
         System.out.println("数あてゲーム");
+        // 0〜9までのint型の乱数を生成
         int ans = new java.util.Random().nextInt(10);
         for (int i = 0; i < 5; i++) {
             System.out.println("0~9の数字を入力してください");
+            // 標準入力から数値を取得
             int num = new java.util.Scanner(System.in).nextInt();
             if (ans == num) {
                 System.out.println("アタリ！");
