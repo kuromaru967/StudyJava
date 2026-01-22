a262b59704ad3c95119c2939281dcd7bf02022ff
knaka <k.nakamura.2003@gmail.com>
2026-01-22 17:37:41 +0900

    4.4.1~4.5.1の履修

---
+++ src/Study4_5_1.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Study4_5_1 {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3};
+        int[] b;
+        b = a;
+        b[0] = 100;
+        System.out.println(a[0]);
+        System.out.println(b[0]);
+    }
+}
