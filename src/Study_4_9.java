6492feea3d156ec916aeca537a21676087e9d00a
knaka <k.nakamura.2003@gmail.com>
2026-02-02 20:56:55 +0900

    4.9の練習問題

---
+++ src/Study_4_9.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Study_4_9 {
+    public static void main(String[] args) {
+        //①
+//        int[] points = new int[4];
+//        double[] weights = new double[4];
+//        boolean[] answers = new boolean[3];
+//        String[] names = new String[3];
+        //②
+//        int[] moneyList = {121902, 8302, 55100};
+//        for (int i = 0; i < moneyList.length; i++) {
+//            System.out.println(moneyList[i]);
+//        }
+//    }
+//}
+//        for (int value : moneyList) {
+//            System.out.println(value);
+//        }
+//    }
+//}
+        //③NullPointerException,ArrayIndexOutOfBoundsException
+        //④
+//        int[] numbers = {3, 4, 9};
+//        System.out.println("一桁の数字を入力して下さい");
+//        int input = new Scanner(System.in).nextInt();
+//        for (int i : numbers) {
+//            if (i == input) {
+//                System.out.println("アタリ！");
+//            }
+//        }
+    }
+}
