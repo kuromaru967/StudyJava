7c180c61cdf488f3a2ec15ee506a65fc793a9d3f
knaka <k.nakamura.2003@gmail.com>
2026-02-17 20:05:29 +0900

    4.9の練習問題のPR修正(コメントアウトを外した)

--- src/Study_4_9.java
+++ src/Study_4_9.java
@@ -12,22 +12,20 @@ public class Study_4_9 {
         for (int i = 0; i < moneyList.length; i++) {
             System.out.println(moneyList[i]);
         }
-    }
-}
         for (int value : moneyList) {
             System.out.println(value);
         }
-    }
-}
-//        ③NullPointerException,ArrayIndexOutOfBoundsException
-        int[] counts = null;
-        float[] heights = {171.3F, 175.0F };
-        System.out.println(counts[1]);
-        System.out.println(heights[2]);
+        //③NullPointerException,ArrayIndexOutOfBoundsException
+//        int[] counts = null;
+//        float[] heights = {171.3F, 175.0F};
+//        System.out.println(counts[1]);
+//        System.out.println(heights[2]);
+
         //④
         boolean found = false;
         int[] numbers = {3, 4, 9};
         System.out.println("一桁の数字を入力して下さい");
+
         int input = new Scanner(System.in).nextInt();
         for (int i : numbers) {
             if (i == input) {
