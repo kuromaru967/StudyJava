75c3bff696edc9a4a5db5b4f94100f00a2408c6d
knaka <k.nakamura.2003@gmail.com>
2026-02-28 17:31:50 +0900

    5.1と5.2の履修

---
+++ src/Study_5_1.java
@@ -0,0 +1,24 @@
+public class Study_5_1 {
+    //    メソッドの呼び出し
+    public static void main(String[] args) {
+        System.out.println("メソッドを呼び出します");
+        hello();                                //メソッド名（引数リスト）
+        System.out.println("メソッドの呼び出しが終わりました");
+    }
+
+    public static void hello() {
+        System.out.println("湊さん、こんにちは");
+    }
+//    public static void methodA() {
+//        System.out.println("methodA");
+//        methodB();                      //methodBの呼び出し
+//    }
+//
+//    public static void methodB() {
+//        System.out.println("methodB");
+//    }
+//
+//    public static void main(String[] args) {
+//        methodA();　　　　　　　　　　　　　//methodAの呼び出し
+//    }
+}
